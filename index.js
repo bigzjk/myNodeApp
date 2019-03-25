cc4096be030a3c7ea41b6792cec586d1e8d92b81
zhujunkun <zhujunkun@yit.com>
2019-03-26 00:13:58 +0800

    拆分成路由模块

--- index.js
+++ index.js
@@ -10,14 +10,12 @@
  *   挂载路由
  *   监听端口启动服务
  */
-const fs = require('fs')
 const express = require('express')
 const bodyParser = require('body-parser')
+const router = require('./router')
 const app = express()
 const port = 3000
 
-// app.use('/public/', express.static('./public/'))
-// app.use('/asset/', express.static('./public/'))
 app.use('/node_modules/', express.static('./node_modules/'))
 app.use('/public/', express.static('./public/'))
 
@@ -26,52 +24,11 @@ app.use(bodyParser.json())
 
 app.engine('html',require('express-art-template'))
 // app.set('views', 'src')
-let arr = [
-  {
-    name: '张三',
-    info: '我是最帅的最帅的子帅的'
-  }
-]
 
+/* 挂载路由 */
+app.use(router)
 
-app
-  .get('/', (req,res)=>{
-    fs.readFile('./db.json', (err, data)=>{
-      if(err){
-        return res.status(500).send('Server error.')
-      }
-      var students = JSON.parse(data).students
-      res.render('index.html',{
-        students
-      })
-    })
-  })
-  .get('/student',(req,res)=>{
-    res.render('new.html')
-  })
-  .get('/student',(req,res)=>{
-    // res.render('post.html')
-    // arr.unshift(req.body)
-    console.log(req.body);
-    // res.render('post.html')
-    res.redirect('/')
-  })
-  .get('/post',(req,res)=>{
-    res.render('post.html')
-  })
-  .post('/post',(req,res)=>{
-    console.log(req.body);
-    arr.unshift(req.body)
-    // res.render('post.html')
-    res.redirect('list')
-
-  })
-  .get('/list',(req,res)=>{
-    res.render('list.html',{
-      list: arr
-    })
-  })
-
+/* 监听端口启动服务 */
 app.listen(port, ()=>{
   console.log(`Example app listening on port ${port}!`)
 })
---
+++ router.js
@@ -0,0 +1,89 @@
+/*
+ * @Author: alkun
+ * @Date:   2019-03-25 23:06:48
+ * @Last Modified by:   alkun
+ * @Last Modified time: 2019-03-26 00:07:06
+ */
+
+'use strict';
+const fs = require('fs')
+const express = require('express')
+const router = express.Router()
+router
+    .get('/', (req, res) => {
+        fs.readFile('./db.json', (err, data) => {
+            if (err) {
+                return res.status(500).send('Server error.')
+            }
+            var students = JSON.parse(data).students
+            res.render('index.html', {
+                students
+            })
+        })
+    })
+    .get('/student', (req, res) => {
+        res.render('new.html')
+    })
+    .get('/student', (req, res) => {
+        // res.render('post.html')
+        // arr.unshift(req.body)
+        console.log(req.body);
+        // res.render('post.html')
+        res.redirect('/')
+    })
+    .get('/post', (req, res) => {
+        res.render('post.html')
+    })
+    .post('/post', (req, res) => {
+        console.log(req.body);
+        arr.unshift(req.body)
+            // res.render('post.html')
+        res.redirect('list')
+
+    })
+    .get('/list', (req, res) => {
+        res.render('list.html', {
+            list: arr
+        })
+    })
+
+module.exports = router
+/*module.exports = function(app) {
+    app
+        .get('/', (req, res) => {
+            fs.readFile('./db.json', (err, data) => {
+                if (err) {
+                    return res.status(500).send('Server error.')
+                }
+                var students = JSON.parse(data).students
+                res.render('index.html', {
+                    students
+                })
+            })
+        })
+        .get('/student', (req, res) => {
+            res.render('new.html')
+        })
+        .get('/student', (req, res) => {
+            // res.render('post.html')
+            // arr.unshift(req.body)
+            console.log(req.body);
+            // res.render('post.html')
+            res.redirect('/')
+        })
+        .get('/post', (req, res) => {
+            res.render('post.html')
+        })
+        .post('/post', (req, res) => {
+            console.log(req.body);
+            arr.unshift(req.body)
+                // res.render('post.html')
+            res.redirect('list')
+
+        })
+        .get('/list', (req, res) => {
+            res.render('list.html', {
+                list: arr
+            })
+        })
+}*/
