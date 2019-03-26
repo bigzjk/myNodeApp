601ba1b520a322a01ba9d63af523f007e348470a
zhujunkun <zhujunkun@yit.com>
2019-03-26 10:00:50 +0800

    提取出对数据增删改查api

--- router.js
+++ router.js
@@ -2,12 +2,13 @@
  * @Author: alkun
  * @Date:   2019-03-25 23:06:48
  * @Last Modified by:   alkun
- * @Last Modified time: 2019-03-26 00:07:06
+ * @Last Modified time: 2019-03-26 09:57:15
  */
 
 'use strict';
 const fs = require('fs')
 const express = require('express')
+const Student = require('./student')
 const router = express.Router()
 router
     .get('/', (req, res) => {
@@ -21,15 +22,29 @@ router
             })
         })
     })
-    .get('/student', (req, res) => {
+    .get('/students/add', (req, res) => {
+        Student.find()
         res.render('new.html')
     })
-    .get('/student', (req, res) => {
+    .post('/students/add', (req, res) => {
+        fs.readFile('./db.json', (err, data) => {
+            if (err) {
+                return res.status(500).send('Server error.')
+            }
+            var students = JSON.parse(data).students
+            students.push(req.body)
+            // fs.writeFile('./db.json', (err, data) => {})
+            res.render('index.html', {
+                students
+            })
+        })
+
+
         // res.render('post.html')
         // arr.unshift(req.body)
         console.log(req.body);
         // res.render('post.html')
-        res.redirect('/')
+        // res.redirect('/')
     })
     .get('/post', (req, res) => {
         res.render('post.html')
---
+++ student.js
@@ -0,0 +1,15 @@
+// 对数据的增删改查操作
+module.exports = {
+    find: ()=>{
+        console.log('find');
+    },
+    save: ()=>{
+        console.log('save');
+    },
+    change: ()=>{
+        console.log('change');
+    },
+    delete: ()=>{
+        console.log('delete');
+    },
+}
