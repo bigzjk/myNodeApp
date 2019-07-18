e0a0d03d6ddb3205d705a5dde6b10bc7f192c8b1
alkun <zhujunkun@yit.com>
2019-07-18 11:39:08 +0800

    feat: 初试api接口转发

--- server/router.js
+++ server/router.js
@@ -7,12 +7,20 @@
 
 'use strict';
 // const fs = require('fs')
+const http = require('http')
 const express = require('express')
 // const Student = require('./student-fs')
 const Student = require('./student')
 const router = express.Router()
+const axios = require('axios')
+
+// let _fn
+let apiHost = 'https://b.zhuishushenqi.com/v2/category/ranklist?node=8c31c6a912464c3e9de4cc6c2c8c402a&type=&packageName=&token='
+// let apiHost = 'https://music.163.com/weapi/appcustomconfig/get'
+
 router
     .get('/', (req, res) => {
+       
         Student.find((err,data)=>{
             if(err){
                 return res.status(500).send('Server error.')
@@ -22,6 +30,20 @@ router
             })
         })
     })
+    .get('/zhuishu', async (req, res, next) => {
+        // axios.defaults.headers['Referer'] = 'Https://m.damai.cn/damai/home/index.html'
+        let resp = await axios.get(apiHost)
+        res.send(resp.data)
+       
+    })
+    //转发 post 请求
+    // .post('/', function(req, res, next){
+    //     var path = req.originalUrl;
+    //     var content = req.body;
+    //     _fn.postData(path, content, function(data){
+    //         res.send(data);
+    //     })
+    // })
     .get('/students/add', (req, res) => {
         res.render('new.html')
     })
@@ -50,5 +72,4 @@ router
             res.redirect('/')
         });
     })
-
 module.exports = router
